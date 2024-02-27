63316e216df4a4dc3c73a44a3a01450b5e004f69
mafortthiago <thmafort@gmail.com>
2024-02-27 13:51:16 -0300

    feat: Add connection with db and create models

--- config/db.js
+++ config/db.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const connection = async () => {
+    try {
+        const dbConnection = await mongoose.connect(
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.c6b4zri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        );
+        return dbConnection;
+    } catch (e) {}
+};
+
+connection();
+module.exports = connection;
---
+++ model/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        name: String,
+        email: String,
+        password: String,
+        image: String,
+        bio: String,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
