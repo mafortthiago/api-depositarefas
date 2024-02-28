dbde89dacd19bbc1caa78b07ac08c2533f23ca52
mafortthiago <thmafort@gmail.com>
2024-02-28 13:45:11 -0300

    feat: Create user registration

--- controller/UserController.js
+++ controller/UserController.js
@@ -7,6 +7,32 @@ const generateToken = (id) => {
     return jwt.sign({ id }, jwtSecret, { expiresIn: "15d" });
 };
 
-const register = async (req, res) => {};
+const register = async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+        res.status(422).json({ errors: ["Email já cadastrado."] });
+        return;
+    }
+    const passwordEncrypted = await bcrypt.hash(
+        password,
+        await bcrypt.genSalt()
+    );
+
+    const newUser = User.create({
+        name,
+        email,
+        password: passwordEncrypted,
+    });
+
+    if (!newUser) {
+        res.status(422).json({ errors: ["Erro, tente novamente!"] });
+        return;
+    }
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
+};
 
 module.exports = { register };
