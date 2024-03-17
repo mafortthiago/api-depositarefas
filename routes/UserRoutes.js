46636e2baf72e2d78e9298400c267ad30a090b83
mafortthiago <126981586+mafortthiago@users.noreply.github.com>
2024-03-17 12:02:30 -0300

    refactor: update route order

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -8,17 +8,21 @@ const {
     update,
     getUserById,
 } = require("../controller/UserController");
-const { imageUpload } = require("../middlewares/imageUpload");
 const validate = require("../middlewares/handleValidation");
-const { authGuard } = require("../middlewares/authGuard");
 const {
     userCreateValidation,
     userLoginValidation,
     userUpdateValidation,
 } = require("../middlewares/userValidations");
+const { authGuard } = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
+
+
+
 router.post("/register", userCreateValidation(), validate, register);
-router.post("/login", userLoginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.post("/login", userLoginValidation(), validate, login);
+
 
 router.put(
     "/",
