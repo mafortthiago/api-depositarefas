1511d41906fad9477a8ad54b752c193c1d4cf65c
mafortthiago <126981586+mafortthiago@users.noreply.github.com>
2024-03-17 15:04:30 -0300

    fix: Update cors config

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 require("./config/db.js");
