eb63e040fc757a60907f6af29dbdd7162623fd20
mafortthiago <126981586+mafortthiago@users.noreply.github.com>
2024-03-17 15:29:57 -0300

    refactor: Update config cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 require("./config/db.js");
