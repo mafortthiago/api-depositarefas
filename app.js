66226c66f665436449da8c983b6a5360319a7849
mafortthiago <126981586+mafortthiago@users.noreply.github.com>
2024-03-18 13:27:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 require("./config/db.js");
